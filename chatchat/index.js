ae4ed65419f35351de6b6dc6249be0625a12c745
Ray <lens1989@gmail.com>
2016-04-01 16:04:12 +0800

    edit chatchat

--- chatchat/index.js
+++ chatchat/index.js
@@ -10,7 +10,10 @@ http.listen(port, function(){
 
 app.use(express.static(__dirname + '/public'));
 
+var numUsers = 0;
+
 io.on('connection', function(socket){
+	var addedUser = false;
 
 	socket.on('new message', function(data) {
 		socket.broadcast.emit('new message', {
@@ -19,7 +22,30 @@ io.on('connection', function(socket){
 		});
 	});
 
+	socket.on('add user', function(username) {
+		if(addedUser) return;
+
+		socket.username = username;
+		++numUsers;
+		addedUser = true;
+		socket.emit('login', {
+			numUsers: numUsers
+		});
+
+		socket.broadcast.emit('user joined', {
+			username: socket.username,
+			numUsers: numUsers
+		});
+	});
+
 	socket.on('disconnect', function(){
-		console.log('user disconnected');
+		if(addedUser) {
+			--numUsers;
+
+			socket.broadcast.emit('user left', {
+				username: socket.username,
+				numUsers: numUsers
+			});
+		}
 	});
 });
